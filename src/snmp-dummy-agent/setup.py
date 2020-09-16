1d9a1fda465bf1e731e17f626349c9f288e83136
raven <orange1st>
2020-09-16 09:48:48 +0900

    snmp dummy agent

---
+++ src/snmp-dummy-agent/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="snmp-dummy-agent",
+    version="0.5.0",
+    author="Duk Kyu Lim",
+    description="snmp dummy agent",
+    license="MIT",
+    keywords="snmp agent",
+    packages=['.'],
+    package_data={
+    },
+    classifiers=[
+        "Development Status :: 1 - Op",
+        "Topic :: Utilities",
+    ],
+)
